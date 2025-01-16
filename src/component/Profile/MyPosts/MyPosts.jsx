62f6ca9fb715320231ffaa4569297e3a6a2678c5
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-01-16 15:23:19 +0300

    01_16.01

--- src/component/Profile/MyPosts/MyPosts.jsx
+++ src/component/Profile/MyPosts/MyPosts.jsx
@@ -17,7 +17,8 @@ function MyPosts(props) {
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    props.dispatch({ type: "UPDATE-NEW-POST-TEXT", newText: text });
+    let action = { type: "UPDATE-NEW-POST-TEXT", newText: text };
+    props.dispatch(action);
   };
 
   return (
