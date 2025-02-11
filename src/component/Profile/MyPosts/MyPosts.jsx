b0ea7d754b70986dcac0f0b86f85a17a5575495a
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-02-11 10:57:26 +0300

    01_11.02

--- src/component/Profile/MyPosts/MyPosts.jsx
+++ src/component/Profile/MyPosts/MyPosts.jsx
@@ -2,25 +2,25 @@ import React from "react";
 import { useRef } from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
-import {
-  updateNewPostTextActionCreator,
-  addPostActionCreator,
-} from "../../../redux/profileReducer";
+// import {
+//   updateNewPostTextActionCreator,
+//   addPostActionCreator,
+// } from "../../../redux/profileReducer";
 
 function MyPosts(props) {
   let postsElements = props.posts.map((post) => (
-    <Post likesCount={post.likesCount} message={post.message} key={post.id} />
+    <Post message={post.message} likesCount={post.likesCount} />
   ));
+
   let newPostElement = useRef();
 
-  let addPost = () => {
-    props.dispatch(addPostActionCreator());
+  let onAddPost = () => {
+    props.addPost();
   };
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
-
-    props.dispatch(updateNewPostTextActionCreator(text));
+    props.updateNewPostText(text);
   };
 
   return (
@@ -36,7 +36,7 @@ function MyPosts(props) {
         </div>
 
         <div>
-          <button onClick={addPost}>Add post</button>
+          <button onClick={onAddPost}>Add post</button>
         </div>
       </div>
       <div className={s.posts}>{postsElements}</div>
---
+++ src/component/Profile/MyPosts/MyPostsContainer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import MyPosts from "./MyPosts";
+import {
+  updateNewPostTextActionCreator,
+  addPostActionCreator,
+} from "../../../redux/profileReducer";
+
+function MyPostsContainer(props) {
+  let state = props.store.getState();
+
+  let addPost = () => {
+    props.store.dispatch(addPostActionCreator());
+  };
+
+  let onPostChange = (text) => {
+    let action = updateNewPostTextActionCreator(text);
+
+    props.store.dispatch(action);
+  };
+
+  return (
+    <MyPosts
+      updateNewPostText={onPostChange}
+      addPost={addPost}
+      posts={state.profilePage.posts}
+      newPostText={state.profilePage.newPostText}
+    />
+  );
+}
+export default MyPostsContainer;
