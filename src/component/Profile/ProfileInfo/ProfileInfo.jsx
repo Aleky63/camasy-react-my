21428f24112d94589e353e2d088721dd916c2a8e
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-04-04 11:34:48 +0300

    02_04.04

--- src/component/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/component/Profile/ProfileInfo/ProfileInfo.jsx
@@ -8,7 +8,7 @@ function ProfileInfo(props) {
         <img src="https://azur.ru/data/newfotos1/big/60/157960.jpg" alt="" />
       </div>
       <div className={s.descr}>
-        В исследованиях, разработке и строительстве Tansuo-3
+        В исследованиях, разработке и строительстве ЕЙСК-3
       </div>
     </div>
   );
